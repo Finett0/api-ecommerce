082c3de84247610962e60f9dedd04c756672f2a4
Finett0 <finettogs@gmail.com>
2025-06-14 16:37:53 -0300

    create endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Bem vindo ao meu servidor Express!");
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Bem vindo ao meu servidor Express! - tsc-watca");
+});
+
+let usuarios = [
+  {
+    nome: "Giovanni Finetto",
+    idade: 21,
+  },
+  {
+    nome: "Erika Buonopane",
+    idade: 37,
+  },
+];
+
+app.get("/users", (req: Request, res: Response) => {
+  res.send(usuarios);
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  let user = req.body;
+  usuarios.push(user);
+  res.send({
+    message: "Usuário criado com sucesso!",
+  });
 });
 
 app.listen(3000, () => {
